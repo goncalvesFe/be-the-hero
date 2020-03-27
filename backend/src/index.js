88d00746a9eae1c44333a2c31c7dae46d940e81a
goncalvesFe <62034737+goncalvesFe@users.noreply.github.com>
2020-03-27 19:58:51 -0300

    Aplicação - BeTheHero

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate'); //sem isso a validação do celebrate retorna um status 500
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 var port = 3333;
 app.listen(port, () => {
